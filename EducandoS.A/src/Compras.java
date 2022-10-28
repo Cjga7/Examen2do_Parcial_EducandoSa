9a5fec244e6bae520f32eb9480ceef555e6e8c51
Cjga7 <cristiangarciaalanis6@gmail.com>
2022-10-28 10:47:49 -0400

    examen2doparcial

---
+++ EducandoS.A/src/Compras.java
@@ -0,0 +1,15 @@
+public class Compras {
+     String jefe_Area;
+
+    Compras(String jefe_Area) {
+        this.setJefe_Area(jefe_Area);
+    }
+
+     String getJefe_Area() {
+        return jefe_Area;
+    }
+
+    public void setJefe_Area(String jefe_Area) {
+        this.jefe_Area = jefe_Area;
+    }
+}
